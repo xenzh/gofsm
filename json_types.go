c74f11f5a345b66f4116154ce8b87f31d1e9a92f
xenzh <xenzh0@gmail.com>
2017-09-21 00:26:18 +0300

    fix that damn tag once again

--- json_types.go
+++ json_types.go
@@ -80,7 +80,7 @@ func (ja *JsonAction) PackagedAction(actions ActionMap) (pa *PackagedAction, err
 }
 
 type JsonTransition struct {
-	ToState string     `bson:"to",json:"to"`
+	ToState string     `bson:"to" json:"to"`
 	Guard   JsonGuard  `json:"guard"`
 	Action  JsonAction `json:"action"`
 }
